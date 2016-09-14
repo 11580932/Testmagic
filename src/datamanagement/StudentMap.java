fff30deaec3b20672c53b9e6233cbd16946ea500
11580932 <hari786raj@gmail.com>
2016-09-14 19:15:09 +1000

    File Format changed

--- src/datamanagement/StudentMap.java
+++ src/datamanagement/StudentMap.java
@@ -1,7 +1,9 @@
 package datamanagement;
+
 public class StudentMap extends java.util.HashMap<Integer, IStudent> {
 
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1;}
+	private static final long serialVersionUID = 1;
+}
